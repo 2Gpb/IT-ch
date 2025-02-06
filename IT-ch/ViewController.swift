e82ec5f4892f114d60d5443829d85e641bd15dcd
2Gpb <Pvtyukhmenev@gmail.com>
2025-02-06 13:54:39 +0300

    create project

---
+++ IT-ch/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  IT-ch
+//
+//  Created by Peter on 06.02.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
