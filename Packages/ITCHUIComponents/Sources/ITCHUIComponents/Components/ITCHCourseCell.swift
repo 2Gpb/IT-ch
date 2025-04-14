84e7596ae6c139e7de2d42c6a6546066af0c2a35
2Gpb <Pvtyukhmenev@gmail.com>
2025-04-14 16:38:17 +0300

    add course cell model

--- Packages/ITCHUIComponents/Sources/ITCHUIComponents/Components/ITCHCourseCell.swift
+++ Packages/ITCHUIComponents/Sources/ITCHUIComponents/Components/ITCHCourseCell.swift
@@ -16,27 +16,50 @@ public final class ITCHCourseCell: UIView {
         
         enum View {
             static let cornerRadius: CGFloat = 12
+            static let backgroundColor = ITCHColor.backgroundGray.color
         }
         
         enum Info {
+            static let font: UIFont = ITCHFont.bodySMedium.font
+            static let textColor: UIColor = ITCHColor.base30.color
+            static let axis: NSLayoutConstraint.Axis = .horizontal
+            static let distribution: UIStackView.Distribution = .equalSpacing
             static let rolePrefix: String = "Роль: "
             static let topOffset: CGFloat = 16
             static let horizontalOffset: CGFloat = 20
         }
         
         enum CourseName {
+            static let font: UIFont = ITCHFont.bodyMMedium.font
+            static let textColor: UIColor = ITCHColor.base0.color
+            static let numberOfLines: Int = 0
             static let topOffset: CGFloat = 20
             static let horizontalOffset: CGFloat = 20
-            static let chevronSize: CGFloat = 16
+        }
+        
+        enum Chevron {
+            static let image: UIImage = ITCHImage.arrowRightLight16.image
+            static let size: CGFloat = 16
+            static let topOffset: CGFloat = 20
+            static let trailingOffset: CGFloat = 20
         }
         
         enum TeacherName {
+            static let font: UIFont = ITCHFont.bodySMedium.font
+            static let textColor: UIColor = ITCHColor.base30.color
+            static let axis: NSLayoutConstraint.Axis = .horizontal
             static let topOffset: CGFloat = 16
             static let horizontalOffset: CGFloat = 20
             static let bottomOffset: CGFloat = 16
-            static let photoSize: CGFloat = 32
-            static let photoCornerRadius: CGFloat = 16
             static let spacing: CGFloat = 8
+        }
+        
+        enum Avatar {
+            static let size: CGFloat = 32
+            static let cornerRadius: CGFloat = 16
+            static let backgroundColor = ITCHColor.backgroundDark.color
+            static let clipToBounds: Bool = true
+            static let contentMode: UIView.ContentMode = .scaleAspectFill
         }
     }
     
@@ -46,27 +69,25 @@ public final class ITCHCourseCell: UIView {
     private let infoStack: UIStackView = UIStackView()
     private let courseNameLabel: UILabel = UILabel()
     private let chevronImage: UIImageView = UIImageView()
-    private let courseNameStack: UIStackView = UIStackView()
-    private let photoImageView: UIImageView = UIImageView()
+    private let avatarImageView: UIImageView = UIImageView()
     private let teacherNameLabel: UILabel = UILabel()
     private let teacherNameStack: UIStackView = UIStackView()
     
+    // MARK: - Properties
+    public var content: ITCHCourseCellModel? {
+        didSet {
+            durationLabel.text = content?.duration
+            roleLabel.text = Constant.Info.rolePrefix + (content?.role ?? "")
+            courseNameLabel.text = content?.courseName
+            teacherNameLabel.text = content?.teacherName
+            avatarImageView.image = content?.avatar
+        }
+    }
+    
     // MARK: - Lifecycle
-    public init(
-        duration: String,
-        role: String,
-        courseName: String,
-        teacherName: String,
-        image: UIImage
-    ) {
+    public init() {
         super.init(frame: .zero)
-        setUp(
-            duration: duration,
-            role: role,
-            courseName: courseName,
-            teacherName: teacherName,
-            image: image
-        )
+        setUp()
     }
     
     @available(*, unavailable)
@@ -75,77 +96,80 @@ public final class ITCHCourseCell: UIView {
     }
     
     // MARK: - SetUp
-    private func setUp(
-        duration: String,
-        role: String,
-        courseName: String,
-        teacherName: String,
-        image: UIImage
-    ) {
-        backgroundColor = ITCHColor.backgroundGray.color
-        layer.cornerRadius = Constant.View.cornerRadius
-        
-        durationLabel.text = duration
-        roleLabel.text = Constant.Info.rolePrefix + role
-        courseNameLabel.text = courseName
-        teacherNameLabel.text = teacherName
-        
+    private func setUp() {
+        setUpView()
         setUpInfoStack()
-        setUpCourseNameStack()
+        setUpChevronImage()
+        setUpCourseNameLabel()
         setUpTeacherNameStack()
+    }
+    
+    private func setUpView() {
+        backgroundColor = Constant.View.backgroundColor
+        layer.cornerRadius = Constant.View.cornerRadius
     }
     
     private func setUpInfoStack() {
         [durationLabel, roleLabel].forEach { label in
-            label.font = ITCHFont.bodySMedium.font
-            label.textColor = ITCHColor.base30.color
+            label.font = Constant.Info.font
+            label.textColor = Constant.Info.textColor
             infoStack.addArrangedSubview(label)
         }
         
-        infoStack.axis = .horizontal
-        infoStack.distribution = .fill
+        infoStack.axis = Constant.Info.axis
+        infoStack.distribution = Constant.Info.distribution
         
         addSubview(infoStack)
         infoStack.pinTop(to: self, Constant.Info.topOffset)
         infoStack.pinHorizontal(to: self, Constant.Info.horizontalOffset)
     }
     
-    private func setUpCourseNameStack() {
-        courseNameLabel.font = ITCHFont.bodyMMedium.font
-        courseNameLabel.textColor = ITCHColor.base0.color
+    private func setUpChevronImage() {
+        chevronImage.image = Constant.Chevron.image
+        chevronImage.setWidth(Constant.Chevron.size)
+        chevronImage.setHeight(Constant.Chevron.size)
         
-        chevronImage.image = ITCHImage.arrowRightLight16.image
-        chevronImage.setWidth(Constant.CourseName.chevronSize)
-        chevronImage.setHeight(Constant.CourseName.chevronSize)
+        addSubview(chevronImage)
+        chevronImage.pinTop(to: infoStack.bottomAnchor, Constant.Chevron.topOffset)
+        chevronImage.pinRight(to: self, Constant.Chevron.trailingOffset)
+    }
+    
+    private func setUpCourseNameLabel() {
+        courseNameLabel.font = Constant.CourseName.font
+        courseNameLabel.textColor = Constant.CourseName.textColor
+        courseNameLabel.numberOfLines = Constant.CourseName.numberOfLines
         
-        courseNameStack.addArrangedSubview(courseNameLabel)
-        courseNameStack.addArrangedSubview(chevronImage)
-        courseNameStack.axis = .horizontal
-        courseNameStack.spacing = 20
-        
-        addSubview(courseNameStack)
-        courseNameStack.pinTop(to: infoStack.bottomAnchor, Constant.CourseName.topOffset)
-        courseNameStack.pinHorizontal(to: self, Constant.CourseName.horizontalOffset)
+        addSubview(courseNameLabel)
+        courseNameLabel.pinTop(to: infoStack.bottomAnchor, Constant.CourseName.topOffset)
+        courseNameLabel.pinLeft(to: self, Constant.CourseName.horizontalOffset)
+        courseNameLabel.pinRight(to: chevronImage.leadingAnchor, Constant.CourseName.horizontalOffset)
     }
     
     private func setUpTeacherNameStack() {
-        photoImageView.backgroundColor = ITCHColor.backgroundDark.color
-        photoImageView.layer.cornerRadius = Constant.TeacherName.photoCornerRadius
-        photoImageView.setWidth(Constant.TeacherName.photoSize)
-        photoImageView.setHeight(Constant.TeacherName.photoSize)
+        setUpTeacherNameStackContent()
         
-        teacherNameLabel.font = ITCHFont.bodySMedium.font
-        teacherNameLabel.textColor = ITCHColor.base30.color
+        [avatarImageView, teacherNameLabel].forEach { element in
+            teacherNameStack.addArrangedSubview(element)
+        }
         
-        teacherNameStack.addArrangedSubview(photoImageView)
-        teacherNameStack.addArrangedSubview(teacherNameLabel)
-        teacherNameStack.axis = .horizontal
-        teacherNameStack.alignment = .center
+        teacherNameStack.axis = Constant.TeacherName.axis
         teacherNameStack.spacing = Constant.TeacherName.spacing
         
         addSubview(teacherNameStack)
-        teacherNameStack.pinTop(to: courseNameStack.bottomAnchor, Constant.TeacherName.topOffset)
+        teacherNameStack.pinTop(to: courseNameLabel.bottomAnchor, Constant.TeacherName.topOffset)
         teacherNameStack.pinHorizontal(to: self, Constant.TeacherName.horizontalOffset)
         teacherNameStack.pinBottom(to: self, Constant.TeacherName.bottomOffset)
+    }
+    
+    private func setUpTeacherNameStackContent() {
+        avatarImageView.backgroundColor = Constant.Avatar.backgroundColor
+        avatarImageView.layer.cornerRadius = Constant.Avatar.cornerRadius
+        avatarImageView.clipsToBounds = Constant.Avatar.clipToBounds
+        avatarImageView.contentMode = Constant.Avatar.contentMode
+        avatarImageView.setWidth(Constant.Avatar.size)
+        avatarImageView.setHeight(Constant.Avatar.size)
+        
+        teacherNameLabel.font = Constant.TeacherName.font
+        teacherNameLabel.textColor = Constant.TeacherName.textColor
     }
 }
