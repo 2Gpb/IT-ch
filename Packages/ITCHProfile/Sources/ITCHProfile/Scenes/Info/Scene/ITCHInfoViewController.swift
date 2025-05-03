389ced478c8051f745ccedafdad7730c644571c1
Petr Tyukhmenev <Pvtyukhmenev@gmail.com>
2025-05-03 20:23:49 +0300

    add text view on info screen

--- Packages/ITCHProfile/Sources/ITCHProfile/Scenes/Info/Scene/ITCHInfoViewController.swift
+++ Packages/ITCHProfile/Sources/ITCHProfile/Scenes/Info/Scene/ITCHInfoViewController.swift
@@ -14,6 +14,22 @@ final class ITCHInfoViewController: UIViewController {
         enum Error {
             static let message = "init(coder:) has not been implemented"
         }
+        
+        enum View {
+            static let backgroundColor: UIColor = ITCHColor.backgroundGray.color
+        }
+        
+        enum NavigationBar {
+            static let leftImage: UIImage = ITCHImage.chevronLeft24.image
+        }
+        
+        enum Text {
+            static let defaultText: String = "\n"
+            static let backgroundColor: UIColor = .clear
+            static let textColor: UIColor = ITCHColor.base0.color
+            static let font: UIFont = ITCHFont.bodyMMedium.font
+            static let insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        }
     }
     
     // MARK: - Private fields
@@ -21,6 +37,7 @@ final class ITCHInfoViewController: UIViewController {
     
     // MARK: - UI Components
     private let navigationBar: ITCHNavigationBar = ITCHNavigationBar(type: .title)
+    private let textView: UITextView = UITextView()
     
     // MARK: - Lifecycle
     init(interactor: ITCHInfoBusinessLogic) {
@@ -45,15 +62,18 @@ final class ITCHInfoViewController: UIViewController {
             .configure(
                 with: ITCHNavigationBarModel(
                     title: model.title,
-                    leftImage: ITCHImage.chevronLeft24.image
+                    leftImage: Constant.NavigationBar.leftImage
                 )
             )
+        
+        textView.text += model.body
     }
     
     // MARK: - SetUp
     private func setUp() {
-        view.backgroundColor = ITCHColor.backgroundGray.color
+        view.backgroundColor = Constant.View.backgroundColor
         setUpNavigationBar()
+        setUpTextView()
     }
     
     private func setUpNavigationBar() {
@@ -64,5 +84,18 @@ final class ITCHInfoViewController: UIViewController {
         view.addSubview(navigationBar)
         navigationBar.pinTop(to: view.safeAreaLayoutGuide.topAnchor)
         navigationBar.pinHorizontal(to: view)
+    }
+    
+    private func setUpTextView() {
+        textView.text = Constant.Text.defaultText
+        textView.backgroundColor = Constant.Text.backgroundColor
+        textView.textColor = Constant.Text.textColor
+        textView.font = Constant.Text.font
+        textView.textContainerInset = Constant.Text.insets
+        
+        view.addSubview(textView)
+        textView.pinTop(to: navigationBar.bottomAnchor)
+        textView.pinHorizontal(to: view)
+        textView.pinBottom(to: view.safeAreaLayoutGuide.bottomAnchor)
     }
 }
